c3e71c7d3fe9dfb34ec6e779eaad27aea9edf1ed
邵煜家 <2326064185@qq.com>
2024-08-10 17:56:50 +0800

    优化nemu difftest输出

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,14 +18,13 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-
   if (cpu.pc != ref_r->pc){
-    printf("pc: 0x%x, ref_pc: 0x%x\n", cpu.pc, ref_r->pc);
+    printf("difftest pc reg error: 0x%x, ref: 0x%x\n", cpu.pc, ref_r->pc);
     return false;
   }
   for (int i=0; i<32; i++) {
     if (gpr(i) != ref_r->gpr[i]) {
-      printf("%s: 0x%x, 0x%x\n", reg_name(i), gpr(i), ref_r->gpr[i]);
+      printf("difftest %s reg error: dut: 0x%x, ref: 0x%x\n", reg_name(i), gpr(i), ref_r->gpr[i]);
       return false;
     }
   }
