4b70f0c9ee1b493f75df1984f9a5ee2e048f5982
邵煜家 <2326064185@qq.com>
2024-08-22 12:49:33 +0800

    实现AM_TIMER_UPTIME并修复nemu rtc的bug

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,11 +1,21 @@
 #include <am.h>
 #include <nemu.h>
+#include <stdint.h>
+#include <stdio.h>
 
+uint64_t rtc_start_time;
 void __am_timer_init() {
+  uint32_t* ptr = (uint32_t*)&rtc_start_time;
+  ptr[0] = inl(RTC_ADDR);
+  ptr[1] = inl(RTC_ADDR+4);
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+  uint64_t time;
+  uint32_t* ptr = (uint32_t*)&time;
+  ptr[0] = inl(RTC_ADDR);
+  ptr[1] = inl(RTC_ADDR+4);
+  uptime->us = time - rtc_start_time;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
