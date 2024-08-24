54de2e81fc68de5ff65f25f5a360750f6cf18956
邵煜家 <2326064185@qq.com>
2024-08-25 03:34:41 +0800

    实现am的vga

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -3,13 +3,21 @@
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
+#include <stdio.h>
 void __am_gpu_init() {
+  int i;
+  int w = io_read(AM_GPU_CONFIG).width;
+  int h = io_read(AM_GPU_CONFIG).height;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  uint32_t size = inl(VGACTL_ADDR);
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
+    .width = size>>16, .height = size&0xffff,
     .vmemsz = 0
   };
 }
@@ -17,6 +25,15 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
+  }
+  int w = io_read(AM_GPU_CONFIG).width;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t *t = (uint32_t *)(uintptr_t)(ctl->pixels);
+
+  for (int y_count = 0; y_count < ctl->h; y_count++) {
+    for (int x_count = 0; x_count < ctl->w; x_count++) {
+      fb[((ctl->y+y_count)*w+ctl->x+x_count)] = t[y_count*w+x_count];
+    }
   }
 }
 
