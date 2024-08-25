edb61e0a86dc0257aee62fea3d73811e31fee9ee
邵煜家 <2326064185@qq.com>
2024-08-26 03:01:03 +0800

    修复TARGET_AM时无法编译的bug

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -41,7 +41,9 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
+#ifndef CONFIG_TARGET_AM
   if (check_wp()) {nemu_state.state = NEMU_STOP;}
+#endif
 }
 
 
