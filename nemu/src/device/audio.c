96c6f16c063972687c1927991790b5be4281eb5b
邵煜家 <2326064185@qq.com>
2024-08-26 02:03:13 +0800

    nemu的声卡

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -30,7 +30,41 @@ enum {
 static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
 
+void audio_callback(void *userdata, Uint8 *stream, int len) {
+  memset(stream, 0, len);
+  if (audio_base[reg_count] > len) {
+    memcpy(stream, sbuf, len);
+
+    memmove(sbuf, sbuf+len, audio_base[reg_count] - len);
+    audio_base[reg_count] -= len;
+  } else {
+    memcpy(stream, sbuf, audio_base[reg_count]);
+    audio_base[reg_count] = 0;
+  }
+}
+
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
+  static SDL_AudioSpec s = {0};
+  if (is_write && offset == reg_init && s.format != AUDIO_S16SYS && audio_base[reg_init] == 1) {
+    s.format = AUDIO_S16SYS;  // 假设系统中音频数据的格式总是使用16位有符号数来表示
+    s.userdata = NULL;        // 不使用
+    s.freq = audio_base[reg_freq];
+    s.channels = audio_base[reg_channels];
+    s.samples = audio_base[reg_samples];
+    s.callback = audio_callback;
+    SDL_InitSubSystem(SDL_INIT_AUDIO);
+    if (SDL_OpenAudio(&s, NULL) != 0) fprintf(stdout, "Failed to open audio: %s\n", SDL_GetError());
+    SDL_PauseAudio(0);
+  }
+  if (offset == reg_sbuf_size*4) {
+    audio_base[reg_sbuf_size] = CONFIG_SB_SIZE;
+  }
+  // if (offset == reg_count*4) {
+  //   audio_base[reg_count] = 0;
+  // }
+
+  
+
 }
 
 void init_audio() {
