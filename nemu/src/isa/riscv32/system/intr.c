6a6a87236daec9c281542c95bb4569f43bdbc49c
邵煜家 <2326064185@qq.com>
2024-08-28 01:20:23 +0800

    在nemu中实现了简单的ecall指令

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -16,11 +16,10 @@
 #include <isa.h>
 
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
-  /* TODO: Trigger an interrupt/exception with ``NO''.
-   * Then return the address of the interrupt/exception vector.
-   */
+  cpu.csr.mepc = epc;
+  cpu.csr.mcause = 0xb;
+  return cpu.csr.mtvec;
 
-  return 0;
 }
 
 word_t isa_query_intr() {
